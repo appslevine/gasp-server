61ea221a3f770bd8b265fd5f56bd905aa2ea469b
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-03-07 16:47:50 -0800

    Completed CRUD

--- createSome.sh
+++ createSome.sh
@@ -2,3 +2,11 @@
 URL=http://localhost:8080
 curl -d '{"name":"bar","url":"http://www.yahoo.com"}' -H "Content-Type: application/json" $URL/restaurants
 curl $URL/restaurants/1
+
+echo "\nUpdating"
+curl -X PUT -d '{"name":"pub","url":"http://www.yahoo.com"}' -H "Content-Type: application/json" $URL/restaurants/1
+curl $URL/restaurants/1
+
+echo "\nDeleting"
+curl -X DELETE $URL/restaurants/1
+curl $URL/restaurants/1
--- src/main/java/com/cloudbees/gasp/resources/RestaurantResource.java
+++ src/main/java/com/cloudbees/gasp/resources/RestaurantResource.java
@@ -6,11 +6,14 @@ import com.google.inject.persist.Transactional;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.*;
 
@@ -27,6 +30,26 @@ public class RestaurantResource {
     @Path("{id}")
     public Restaurant get(@PathParam("id") int id) {
         return manager.find(Restaurant.class,id);
+    }
+
+    @PUT
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    @Path("{id}")
+    @Transactional
+    public Restaurant update(@PathParam("id") int id, Restaurant src) {
+        Restaurant r = get(id);
+        r.setUrl(src.getUrl());
+        r.setName(src.getName());
+        return manager.find(Restaurant.class,id);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response update(@PathParam("id") int id) {
+        manager.remove(get(id));
+        return Response.ok().build();
     }
 
     @POST
